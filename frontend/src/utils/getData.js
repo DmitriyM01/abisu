676511db3e455243f439b982434cd89b713122ce
DmitriyM01 <mironowd697@gmail.com>
2025-05-09 00:29:17 -0500

    deploy

--- frontend/src/utils/getData.js
+++ frontend/src/utils/getData.js
@@ -1,6 +1,9 @@
 import axios from "axios";
+const url = import.meta.env.VITE_API_URL;
+
+console.log(url)
 
 export const getData = async () => {
-	const products = (await axios.get('http://localhost:5000/products')).data;
+	const products = (await axios.get(url + '/products')).data;
 	return products;
 };
